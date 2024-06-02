1f0b43d2ddad865448e2916170e5aea6d6b4ff29
johann-miller <johann.miller@protonmail.com>
2024-06-02 16:27:22 -0400

    Added file structure for content loading from a .json file for blog posts. Example of import on home page file.

--- src/lib/renderMath.js
+++ src/lib/renderMath.js
@@ -1,20 +1,20 @@
 export function renderMath(node) {
-    renderMathInElement(node, {
-      delimiters: [
-        { left: "$$", right: "$$", display: true },
-        { left: "$", right: "$", display: false },
-        { left: "\\(", right: "\\)", display: false },
-        { left: "\\[", right: "\\]", display: true },
-      ],
-      throwOnError: false,
-    });
-  
-    return {
-      update(newParams) {
-        renderMathInElement(node, newParams);
-      },
-      destroy() {
-        // Cleanup if necessary when the element is removed from the DOM
-      }
-    };
-  }
+  renderMathInElement(node, {
+    delimiters: [
+      { left: "$$", right: "$$", display: true },
+      { left: "$", right: "$", display: false },
+      { left: "\\(", right: "\\)", display: false },
+      { left: "\\[", right: "\\]", display: true },
+    ],
+    throwOnError: false,
+  });
+
+  return {
+    update(newParams) {
+      renderMathInElement(node, newParams);
+    },
+    destroy() {
+      // Cleanup if necessary when the element is removed from the DOM
+    }
+  };
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,12 @@
 <script>
     import { renderMath } from "$lib/renderMath.js"
+    import contentData from '../data/content.json';
+
+    let content = contentData.content;
 </script>
 
 <h1>Drew's mathematics blog</h1>
 
 <div use:renderMath>
-    <p>Some math: $g \in G$</p>
+    {@html content}
 </div>
