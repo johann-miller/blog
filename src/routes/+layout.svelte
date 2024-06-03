e880203e0e56eb052c8d358a3c41e88b8400903c
johann-miller <johann.miller@protonmail.com>
2024-06-02 22:11:23 -0400

    Changed stylings on the +layout.svelte file

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script defer>
     import painting from "$lib/assets/painting.jpg";
+    import { renderMath } from "$lib/renderMath.js";
 </script>
 
 <nav>
@@ -20,14 +21,12 @@
     <slot></slot>
 </div>
 
-<footer>
-    This is a footer!
-</footer>
+<footer>Errors? Comments? EMAIL <span class="dash">-</span> Copyright Drew, 2024</footer>
 
 <style>
     h1 {
-        margin: 1rem auto;
         text-align: center;
+        border-left: 3px solid #bebebe;
     }
 
     #header-image {
@@ -41,21 +40,29 @@
         margin: 0 auto 10rem auto;
     }
 
+    .dash {
+        color: #bebebe;
+    }
+
     footer {
         text-align: center;
         font-size: 0.8rem;
-        color: #bebebe;
-        padding: 1rem 0;
+        padding: 0;
+        width: 100%;
+        position: sticky;
+        height: 3rem;
+        top: calc(100vh - 3rem);
     }
 
     nav {
+        margin-top: 1rem;
         background-color: default;
         display: flex;
     }
 
     nav ul {
         list-style: none;
-        margin: 1rem 0 0 0;
+        margin: 0.5rem 0 0 0;
         padding: 0;
         display: flex;
         flex: 1;
@@ -64,14 +71,14 @@
     }
 
     nav ul li {
-        padding: 0.5rem;
+        padding: 0.25rem;
     }
 
     nav ul li a {
         color: black;
         text-decoration: none;
         padding: 0.25rem;
-        margin: 0.5rem;
+        margin: 0.25rem;
         border-bottom: 2px solid transparent;
         transition: border-color 0.3s ease;
     }
