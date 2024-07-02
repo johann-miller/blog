f5f94fbebebfdf44b5fe862bd707e1bdc1a61b12
johann-miller <johann.miller@protonmail.com>
2024-07-01 23:48:25 -0400

    Trying documentation files

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,15 @@
 import adapter from '@sveltejs/adapter-static';
 
-const dev = process.argv.includes('dev');
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    kit: {
-        adapter: adapter(),
-        paths: {
-            base: dev ? '' : process.env.BASE_PATH,
-        }
-    }
+	kit: {
+		adapter: adapter({
+			fallback: '404.html'
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
+	}
 };
 
 export default config;
