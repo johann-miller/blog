d1d0cc73af463be27355bc9923848dfcd25f2f7e
johann-miller <johann.miller@protonmail.com>
2024-07-01 23:51:17 -0400

    Changed base name to repo name

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
 
+const dev = process.env.NODE_ENV === 'development';
+const repoName = 'blog'; // Replace 'your-repo-name' with your actual repository name
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
@@ -7,7 +10,7 @@ const config = {
 			fallback: '404.html'
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			base: dev ? '' : `/${repoName}`
 		}
 	}
 };
